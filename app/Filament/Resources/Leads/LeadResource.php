1d62d4f7e8932858c9d5843988ad9d361fe70189
Mohamed Khalifa <5alifa.dev@gmail.com>
2026-06-27 23:13:18 +0300

    feat: update LeadResource and LeadsRelationManager for improved record handling and filtering

--- app/Filament/Resources/Leads/LeadResource.php
+++ app/Filament/Resources/Leads/LeadResource.php
@@ -25,7 +25,7 @@ class LeadResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'Leads';
+    protected static ?string $recordTitleAttribute = 'name';
 
     #[Override]
     public static function form(Schema $schema): Schema
--- app/Filament/Resources/Mailings/RelationManagers/LeadsRelationManager.php
+++ app/Filament/Resources/Mailings/RelationManagers/LeadsRelationManager.php
@@ -1,15 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Filament\Resources\Mailings\RelationManagers;
 
 use App\Filament\Resources\Leads\LeadResource;
+use App\LeadStatus;
 use Filament\Actions\AttachAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DetachAction;
 use Filament\Actions\DetachBulkAction;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class LeadsRelationManager extends RelationManager
@@ -21,9 +21,13 @@ class LeadsRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return $table
+//            ->filters
             ->headerActions([
                 // ...
-                AttachAction::make(),
+                AttachAction::make()
+                    ->preloadRecordSelect()
+                    ->recordSelectSearchColumns(['name', 'email'])
+                    ->multiple()
             ])
             ->recordActions([
                 // ...
@@ -34,6 +38,10 @@ class LeadsRelationManager extends RelationManager
                     // ...
                     DetachBulkAction::make(),
                 ]),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->options(LeadStatus::cases())
             ]);
     }
 }
